ad3a014bb78b5aaa6af080dc4b5ff25425cbb971
Tommerty <86270372+tommerty@users.noreply.github.com>
2023-07-03 13:01:03 +0100

    attempting to build component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/lib/components/ui/button";
+import { Landing } from "@/lib/components/landing";
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>
-                <Button>Click me</Button>
+                <Landing>
             </div>
         </main>
     );
