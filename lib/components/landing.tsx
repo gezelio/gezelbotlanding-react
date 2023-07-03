c27d65179c8062e3a69a400082015ccdb9b25f48
trent-gezel <trent.hopton@gmail.com>
2023-07-03 22:32:47 +1000

    Update landing.tsx

--- lib/components/landing.tsx
+++ lib/components/landing.tsx
@@ -8,7 +8,7 @@ const Landing = () => {
                 <div className="max-w-md">
                     <h1 className="text-5xl font-bold">GezelBot</h1>
                     <h2 className="font-bold text-xl">Is offline for the time being</h2>
-                    <p className="py-6">We're hard at work rebuilding the bot from the ground up and improving our features.</p>
+                    <p className="py-6">We&apos;re hard at work rebuilding the bot from the ground up and improving our features.</p>
                     <p className="py-6">To stay up to date, we invite you to join our Discord server</p>
                     <DiscordButton />
                 </div>
