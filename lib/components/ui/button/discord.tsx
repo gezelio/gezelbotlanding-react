b39c3f655021d8f28d0c3dadfc04245800298eff
trent-gezel <trent.hopton@gmail.com>
2023-07-03 22:28:08 +1000

    added react-icons

--- lib/components/ui/button/discord.tsx
+++ lib/components/ui/button/discord.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { DiscordLogoIcon } from "@radix-ui/react-icons";
+import * as FaIcons from 'react-icons/fa';
 
 const DiscordButton = () => {
     return (
         <a href="https://gezel.io/discord" className="btn bg-[#5765F2] border-0 p-10 content-center">
-            <DiscordLogoIcon className="text-white h-14 w-14" />
+            <FaIcons.FaDiscord className="text-white h-14 w-14" />
         </a>
     );
 };
