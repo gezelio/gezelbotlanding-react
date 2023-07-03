bae92579ff34e2c5ca0d6e81b4354e61fe372cd1
trent-gezel <trent.hopton@gmail.com>
2023-07-03 21:51:01 +1000

    tailwind fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/lib/components/ui/button"
+import { Button } from "@/lib/components/ui/button";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div>
-       <Button>Click me</Button>
-     </div>
-    </main>
-  )
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                <Button>Click me</Button>
+            </div>
+        </main>
+    );
 }
